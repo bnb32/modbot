356e58c4155e2d414e70c8eee5582cea6a4948ae
bnb32 <b.benton19@gmail.com>
2022-07-23 06:34:01 -0700

    test fixes

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,14 +4,14 @@ import os
 import pytest
 import pandas as pd
 
-from modbot.training.models import (LSTM, SVM, CNN, BERT, BertCnnTorch)
+from modbot.training.models import (SVM, BertCnnTorch)
 from modbot import TEST_DATA_DIR
 from modbot.utilities.logging import get_logger
 from modbot.environment import get_model_path
 
 logger = get_logger()
 
-MODELS = [(SVM), (LSTM), (CNN), (BERT), (BertCnnTorch)]
+MODELS = [(SVM), (BertCnnTorch)]
 
 
 @pytest.mark.parametrize('MODEL', MODELS)
